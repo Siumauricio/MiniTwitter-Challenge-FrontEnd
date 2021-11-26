8a9931f1f4b2167bc081f59e1c147238fe421f3b
Siumauricio <Siumauricio@unitec.edu>
2021-11-26 08:23:45 -0600

    Added interface Make Tweets
    This interface show a textarea to inser a tweet.

--- src/app/tweet/tweet.component.ts
+++ src/app/tweet/tweet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tweet',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tweet.component.css']
 })
 export class TweetComponent implements OnInit {
+  TweetForm : FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder,private router: Router) { 
+    this.TweetForm = this.formBuilder.group({
+      tweet: ['',[Validators.required,Validators.maxLength(130)]],
+    });
+  }
 
   ngOnInit(): void {
   }
+  onSubmit(){
+  }
 
+  get tweet(){
+    return this.TweetForm.get('tweet');
+  }
 }
